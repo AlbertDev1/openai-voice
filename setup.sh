0effc3492d95d7505b5d551be59afe8506687827
Rene <classicalbert@gmail.com>
2023-04-09 15:47:30 -0400

    update test setup file

--- setup.sh
+++ setup.sh
@@ -15,23 +15,33 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
         echo "portaudio is not installed. Installing it via Homebrew..."
         brew install portaudio
     fi
-    if ! command -v espeak >/dev/null 2>&1; then
-        echo "espeak command not found. Please install it first."
-        exit 1
+    if ! brew list espeak >/dev/null 2>&1; then
+        echo "espeak is not installed. Installing it via Homebrew..."
+        brew install espeak
     fi
+    echo "Creating virtual environment for macOS..."
+    python3.8 -m venv env
+    source env/bin/activate
     echo "Installing Python dependencies from requirements.txt..."
+    pip install --upgrade pip
     pip install -r requirements.txt
+    echo "All Done ;-)"
     exit 0
 elif [[ "$OSTYPE" == "win32" ]]; then
     echo "Windows detected. Checking for Chocolatey..."
     if ! command -v choco >/dev/null 2>&1; then
         echo "Chocolatey not found. Installing Chocolatey..."
-        Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+        Set-ExecutionPolicy Bypass -Scope Process -Force; iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
     fi
     echo "Installing dependencies via Chocolatey..."
     choco install -y portaudio espeak python
+    echo "Creating virtual environment for Windows..."
+    python3.8 -m venv env
+    env\Scripts\activate.bat
     echo "Installing Python dependencies from requirements.txt..."
+    pip install --upgrade pip
     pip install -r requirements.txt
+    echo "All Done ;-)"
     exit 0
 else
     echo "Unsupported OS detected. Please install the dependencies manually:"
@@ -40,3 +50,4 @@ else
     echo "Python: https://www.python.org/downloads/"
     exit 1
 fi
+
